1009504e642409c78608aa08cb9213f60361bcaa
Ahmed Ibrahim <ah.ib94@hotmail.com>
2025-02-16 20:38:20 +0200

    add throttling on syncing then we don't use all the quota while fetching the data from the api

--- app/Console/Commands/FetchAllCommand.php
+++ app/Console/Commands/FetchAllCommand.php
@@ -3,7 +3,10 @@
 namespace App\Console\Commands;
 
 use App\ApiFetchers\NewsApiFetcher;
+use App\ApiFetchers\NewYorkTimesFetcher;
 use App\ApiFetchers\TheGuardianApiFetcher;
+use App\ApiFetchers\TheGuardianFetcher;
+use App\Models\FetcherNextStatus;
 use Illuminate\Console\Command;
 
 class FetchAllCommand extends Command
@@ -22,6 +25,10 @@ class FetchAllCommand extends Command
      */
     protected $description = 'Fetch News From Apis';
 
+    protected $newsapi_minutes = '20';
+    protected $theguardian_minutes = '1';
+    protected $ny_minutes = '2';
+
     /**
      * Create a new command instance.
      *
@@ -39,8 +46,30 @@ class FetchAllCommand extends Command
      */
     public function handle()
     {
-        $this->call('newsapi:fetch');
-        $this->call('theguardian:fetch');
-        $this->call('newyorktimes:fetch');
+        //100 request per day
+        //i can run this 1 request per 20 minutes so in day 3*24 = 72 and leave rest of 100 for testing..
+        $last_run = FetcherNextStatus::where('key',class_basename(NewsApiFetcher::class))->value('updated_at');
+        $last_run_from_seconds = time() - strtotime($last_run);
+        if($last_run_from_seconds>$this->newsapi_minutes*60){
+            $this->call('newsapi:fetch');
+        }
+
+
+        //5000 request per day
+        //i can run this 1 request per 1 minutes so in day 60*24 = 1,440 and even i left a lot of quota 
+        $last_run = FetcherNextStatus::where('key',class_basename(TheGuardianFetcher::class))->value('updated_at');
+        $last_run_from_seconds = time() - strtotime($last_run);
+        if($last_run_from_seconds>$this->theguardian_minutes*60){
+            $this->call('theguardian:fetch');
+        }
+
+
+        //1000 request per day
+        //i can run this 1 request per 2 minutes so in day 30*24 = 720
+        $last_run = FetcherNextStatus::where('key',class_basename(NewYorkTimesFetcher::class))->value('updated_at');
+        $last_run_from_seconds = time() - strtotime($last_run);
+        if($last_run_from_seconds>$this->ny_minutes*60){
+            $this->call('newyorktimes:fetch');
+        }
     }
 }
