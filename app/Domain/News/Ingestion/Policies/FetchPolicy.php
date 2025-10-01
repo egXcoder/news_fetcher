b585a9d2b98075e0e17ab44785723aa12c0339df
Ahmed Ibrahim <ah.ib94@hotmail.com>
2025-10-01 23:36:52 +0300

    fetch policy doesnt require extra methods as policies is just to check if operation can be done or not

--- app/Domain/News/Ingestion/Policies/FetchPolicy.php
+++ app/Domain/News/Ingestion/Policies/FetchPolicy.php
@@ -3,18 +3,7 @@
 namespace App\Domain\News\Ingestion\Policies;
 
 class FetchPolicy{
-    protected $source_name;
-
-    public function __construct($provider_source_name)
-    {
-        $this->source_name = $provider_source_name;    
-    }
-
     public function canFetch(){
 
     }   
-
-    public function recordLastRunTime(){
-
-    }
 }
