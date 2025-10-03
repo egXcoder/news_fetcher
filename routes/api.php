c3a5e98d15d748ce72440a2cdd54303b22576c51
Ahmed <ah.ib94@hotmail.com>
2025-10-03 22:32:16 +0300

    add swagger package

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/articles', [ArticlesController::class,'getArticles'])->name('articles.get');
+Route::get('/articles/search', [ArticlesController::class,'searchArticles'])->name('articles.search');
