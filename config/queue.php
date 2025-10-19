16a50e91090478102cce8813a40edd2a5e21cbdf
Ahmed Ibrahim <ah.ib94@hotmail.com>
2025-10-19 19:42:16 +0300

    test pushing ci/cd

--- config/queue.php
+++ config/queue.php
@@ -51,7 +51,7 @@ return [
             'after_commit' => false,
         ],
 
-        'sqs' => [
+        'sqs' => [ 
             'driver' => 'sqs',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
